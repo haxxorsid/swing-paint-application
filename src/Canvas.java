187f2180dda328e858b061e00cb6789d28593e3c
Siddhesh Patil <s166865h@gmail.com>
2016-07-31 17:37:33 +0530

    Added save(),load() and BufferedImage variable
    Replaced Image to BufferedImage.
    Added save(), load() method for saving, opening work as Image.

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,23 +1,42 @@
-import javax.swing.JComponent;
-
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
 
 public class Canvas extends JComponent {
 
 	private int X1, Y1, X2, Y2;
-	private Image img;
 	private Graphics2D g;
+	
+	private BufferedImage img;
+
+	public void save(){
+		 try {  
+	       ImageIO.write(img, "PNG", new File("filename.png"));
+	       
+	    } catch (IOException ex) {}
+	 }
+	 
+	public void load() {
+		try {
+			img = ImageIO.read(new File("filename.png"));
+			g = img.createGraphics();
+		} catch (IOException ex) {
+		}
+	}
 
 	protected void paintComponent(Graphics g1) {
 		if (img == null) {
-			img = createImage(getSize().width, getSize().height);
+			img = (BufferedImage) createImage(getSize().width, getSize().height);
 			g = (Graphics2D) img.getGraphics();
 			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 					RenderingHints.VALUE_ANTIALIAS_ON);
